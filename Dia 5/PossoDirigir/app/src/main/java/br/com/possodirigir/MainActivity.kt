4ce159f86ff7aef5a718594976252c2234fe2f8c
Luana Manfredi <luana.manfredib@gmail.com>
2019-04-05 21:52:18 -0300

    Quinta aula

---
+++ Dia 5/PossoDirigir/app/src/main/java/br/com/possodirigir/MainActivity.kt
@@ -0,0 +1,34 @@
+ package br.com.possodirigir
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+ class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Passando nome indicado para a próxima tela quando o botão "Continuar" for tocado
+        btnContinuar.setOnClickListener {
+
+            //Verificando se o campo está preenchido
+            if(edtNome.text.isNotEmpty()){
+
+                val intentTela2 = Intent(this, Tela2::class.java)
+
+                intentTela2.putExtra("NomeUsuario", edtNome.text.toString())
+
+                startActivity(intentTela2)
+
+            } else {
+
+                Toast.makeText(this, "Preencha o campo, por favor.", Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+}
